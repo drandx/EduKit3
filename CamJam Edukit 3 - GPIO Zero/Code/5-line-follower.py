499ed36b658a01ae10599a61ac26a178350debab
Pablo Garcia <garjuanpablo@gmail.com>
2026-02-21 23:37:57 -0500

    Refactor line following logic and improve seekline function

--- CamJam Edukit 3 - GPIO Zero/Code/5-line-follower.py
+++ CamJam Edukit 3 - GPIO Zero/Code/5-line-follower.py
@@ -1,82 +1,110 @@
-# Tested using a robot built from the box the kit came in.
-# Wheels mounted to the front of the robot. Sensor mounted between the wheels so turning arc of the sensor is small.
-from gpiozero import CamJamKitRobot
-from gpiozero import Button
-import time
+# CamJam EduKit 3 - Robotics
+# Worksheet 8 - Line Following Robot
 
+from gpiozero import CamJamKitRobot, LineSensor  # Import GPIO Zero Library
+import time  # Import the Time library
+
+# Set variables for the line detector GPIO pin
 pinLineFollower = 25
-sensor = Button(pinLineFollower)
 
+linesensor = LineSensor(pinLineFollower)
 robot = CamJamKitRobot()
 
-# Return True if the line detector is over a black line
-def IsOverBlack():
-    if sensor.is_pressed:
-        return True
-    else:
-        return False
+# Set the relative speeds of the two motors, between 0.0 and 1.0
+leftmotorspeed = 0.5
+rightmotorspeed = 0.5
+
+motorforward = (leftmotorspeed, rightmotorspeed)
+motorbackward = (-leftmotorspeed, -rightmotorspeed)
+motorleft = (leftmotorspeed, -rightmotorspeed)
+motorright = (-leftmotorspeed, rightmotorspeed)
+
+direction = True      # The direction the robot will turn - True = Left
+isoverblack = True    # A flag to say the robot can see a black line
+linelost = False      # A flag that is set if the line has been lost
+
+
+# Define the functions that will be called when the line is
+# detected or not detected
+def lineseen():
+    global isoverblack, linelost
+    print("The line has been found.")
+    isoverblack = True
+    linelost = False
+    robot.value = motorforward
+
+
+def linenotseen():
+    global isoverblack
+    print("The line has been lost.")
+    isoverblack = False
+
 
 # Search for the black line
-def SeekLine():
+def seekline():
+    global direction, linelost
+    robot.stop()
+
     print("Seeking the line")
-    # The direction the robot will turn - True = Left
-    Direction = True
-    SeekSize = 0.2 # Turn time
-    SeekCount = 1 # A count of times the robot has looked for the line
-    MaxSeekCount = 5 # The maximum time to seek the line in one direction
+
+    seeksize = 0.25      # Turn for 0.25s
+    seekcount = 1        # A count of times the robot has looked for the line
+    maxseekcount = 5     # The maximum time to seek the line in one direction
 
     # Turn the robot left and right until it finds the line
-    # Or it has been searched for long enough
-    while SeekCount <= MaxSeekCount:
+    # Or we have looked long enough
+    while seekcount <= maxseekcount:
         # Set the seek time
-        SeekTime = SeekSize * SeekCount
+        seektime = seeksize * seekcount
 
         # Start the motors turning in a direction
-        if Direction:
+        if direction:
             print("Looking left")
-            robot.left(0.4)
+            robot.value = motorleft
         else:
             print("Looking Right")
-            robot.right(0.4)
+            robot.value = motorright
 
         # Save the time it is now
-        StartTime = time.time()
+        starttime = time.time()
 
-        # While the robot is turning for SeekTime seconds
+        # While the robot is turning for seektime seconds,
         # check to see whether the line detector is over black
-        while time.time() - StartTime <= SeekTime:
-            if IsOverBlack():
-                robot.stop()
+        while (time.time() - starttime) <= seektime:
+            if isoverblack:
+                robot.value = motorforward
+                # Exit the seekline() function returning
+                # True - the line was found
                 return True
 
-        # The robot has not not found the black line yet, so stop
+        # The robot has not found the black line yet, so stop
         robot.stop()
 
-        time.sleep(0.1)
-
         # Increase the seek count
-        SeekCount += 1
+        seekcount += 1
 
         # Change direction
-        Direction = not Direction
+        direction = not direction
 
-    # The line wasn't found
+    # The line wasn't found, so return False
+    robot.stop()
+    print("The line has been lost - relocate your robot")
+    linelost = True
     return False
 
-try:
-    print("Following the line")
-    while True:
-        if IsOverBlack():
-            robot.forward(0.4)
-        else:
-            robot.stop()
-            if SeekLine() == False:
-                robot.stop()
-                print("The robot has lost the line")
-                exit()
-            else:
-                print("Following the line")
 
+# Tell the program what to do when a line is seen
+linesensor.when_line = lineseen
+# And when no line is seen
+linesensor.when_no_line = linenotseen
+
+try:
+    # repeat the next indented block forever
+    robot.value = motorforward
+    while True:
+        if not isoverblack and not linelost:
+            seekline()
+
+# If you press CTRL+C, cleanup and stop
 except KeyboardInterrupt:
-    robot.stop()
     exit()
