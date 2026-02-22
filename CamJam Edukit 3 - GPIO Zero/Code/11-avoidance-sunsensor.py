59f057f717629f1e86b43a57246bf5d4c9cb9090
Pablo Garcia <garjuanpablo@gmail.com>
2026-02-22 14:55:03 -0500

    Adjust drift setting for improved straight movement

--- CamJam Edukit 3 - GPIO Zero/Code/11-avoidance-sunsensor.py
+++ CamJam Edukit 3 - GPIO Zero/Code/11-avoidance-sunsensor.py
@@ -17,7 +17,7 @@ sensor = SunSensor()
 # === Settings you can change ===
 TooClose = 90    # How close to a wall before turning (centimeters)
 Speed = 0.5        # How fast the robot drives (0.0 to 1.0)
-Drift = 0.05       # Make left motor a bit slower so robot goes straight
+Drift = 0.1       # Make left motor a bit slower so robot goes straight
 TurnSpeed = 0.5    # How fast the robot turns (0.0 to 1.0)
 TurnStep = 0.5    # How long each little turn lasts (seconds)
 
