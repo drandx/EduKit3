2c37f3b3bb1570f563f2725667b09bd725b5c0fd
Pablo Garcia <garjuanpablo@gmail.com>
2026-02-21 22:54:53 -0500

    Adjust robot speed and turning speed for improved line following control

--- CamJam Edukit 3 - GPIO Zero/Code/13-line-follower-light-measuring.py
+++ CamJam Edukit 3 - GPIO Zero/Code/13-line-follower-light-measuring.py
@@ -17,9 +17,9 @@ robot = CamJamKitRobot()
 sun = SunSensor()
 
 # How fast the robot moves (0.0 = stopped, 1.0 = full speed)
-speed = 1.0
+speed = 0.5
 # Slower speed for turning when the line is lost
-turn_speed = 0.5
+turn_speed = 0.2
 
 # Keep all light readings so we can calculate an average
 light_readings = []
